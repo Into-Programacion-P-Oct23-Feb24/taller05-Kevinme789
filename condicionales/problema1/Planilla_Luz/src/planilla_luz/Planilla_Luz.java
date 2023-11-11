0084ac13a1037a3f96436978dbcde486faed9381
Kevin Robles <porkevin743@gmail.com>
2023-11-10 19:06:52 -0500

    Actividades

--- condicionales/problema1/Planilla_Luz/src/planilla_luz/Planilla_Luz.java
+++ condicionales/problema1/Planilla_Luz/src/planilla_luz/Planilla_Luz.java
@@ -15,12 +15,7 @@ public class Planilla_Luz {
     /**
      * @param args the command line arguments
      */
-    /*
-    Solución que permita calcular y mostrar el valor a cancelar de una planilla 
-     * de luz. Se debe ingresar el valor de costo por kilovatio/hora y el número 
-     * de kilovatios consumidos en el mes. Si el usuario tiene edad mayor a 65 
-     * años, se debe descontar el 10%.
-    */
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double total;
@@ -29,7 +24,8 @@ public class Planilla_Luz {
         float kilovatiosConsumidos;
         System.out.println("Ingrese el valor de costo por kilovatio/hora");
         costoKilovatio = sc.nextFloat();
-        System.out.println("Ingrese el numero de kilovatios consumidos en el mes");
+        System.out.println("Ingrese el numero de kilovatios consumidos en el "
+                + "mes");
         kilovatiosConsumidos = sc.nextFloat();
         System.out.println("Ingrese su edad");
         edad = sc.nextInt();
@@ -37,11 +33,11 @@ public class Planilla_Luz {
         total = costoKilovatio * kilovatiosConsumidos;
         if (edad > 65) {
             total = total - (total * 0.1);
-            System.out.println("Su total a pagar es: " + total);
+            System.out.println("Su total a pagar con descuento es: " + total);
         } else {
             System.out.println("Su total a pagar es: " + total);
         }
 
     }
-    
+
 }
